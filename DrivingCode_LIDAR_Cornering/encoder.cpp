c62d02680804c37739a017586d388b2575d61111
wreevehicks <wrhicks@bu.edu>
2015-12-08 10:38:40 -0500

    Added encoder dead reckoning

--- DrivingCode_LIDAR_Cornering/encoder.cpp
+++ DrivingCode_LIDAR_Cornering/encoder.cpp
@@ -1,12 +1,25 @@
 #include "encoder.h"
 volatile uint32_t encoder_count = 0;
+uint32_t encoder_distance = 0;
+uint32_t timer_encoder = 0;
 
-void init_encoder(){
-	pinMode(PIN_ENCODER_IN, INPUT);
-	digitalWrite(PIN_ENCODER_IN, HIGH);
-//	attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_IN), encoder_ISR, CHANGE);
+
+void init_encoder() {
+  pinMode(PIN_ENCODER_IN, INPUT);
+  digitalWrite(PIN_ENCODER_IN, HIGH);
+  //	attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_IN), encoder_ISR, CHANGE);
 }
 
 void encoder_ISR() {
-	encoder_count++;
+  encoder_count++;
 }
+
+void encoder_calculate_distance() {
+  if (millis() - timer_encoder > 1000) {
+    encoder_distance = (encoder_count * 0.625) / 72;
+    timer_encoder = millis();
+    Serial.print("Distance: ");
+    Serial.println(encoder_distance);
+  }
+}
+
--- DrivingCode_LIDAR_Cornering/encoder.h
+++ DrivingCode_LIDAR_Cornering/encoder.h
@@ -9,11 +9,15 @@
 
 #define PIN_ENCODER_IN	2
 #define ENCODER_SCALING 833.33 // Convert from pulses/ms to rpm 60000/72
+#define PULSE_TO_DISTANCE
 
 extern volatile uint32_t encoder_count;
+extern uint32_t encoder_distance;
+extern uint32_t timer_encoder;
 
 void init_encoder();
 void read_encoder();
 void encoder_ISR();
+void encoder_calculate_distance();
 
 #endif
